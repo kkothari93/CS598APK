f6483c4612055ace02d3db9d6e1b8831c002be61
kkothari93 <krk3mar@gmail.com>
2017-12-14 14:27:44 -0600

    add input wave class

---
+++ input.py
@@ -0,0 +1,22 @@
+# Input functions
+# Boundary conditions
+
+
+class PlaneWave:
+	"""Implements a plane wave class"""
+
+    def __init__(self, k, w):
+        self.k = k
+        self.w = w
+
+    def u_in(self, pts):
+        return np.exp(1j*self.k*self.w @ pts)
+
+    def grad_u_in(self, pts):
+        return 1j*k*w * u_in(pts, self.k, self.w)
+
+    def f(self, pts, eta):
+        return self.u_in(pts) + 1j*eta*self.grad_u_in(pts)
+
+    def g(self, pts, eta)
+        return self.u_in(pts) + 1j*eta*self.grad_u_in(pts)
